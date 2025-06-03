4c03e8e86edfb7ff5f1626a107749e936a9e6108
championcjs <chandler.siffrain@gmail.com>
2025-06-02 20:54:08 -0400

    Fix errors in variables.tf that TFLint did not like

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "aws_region" {
   description = "AWS region"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "AMI to use for EC2"
-  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI for us-east-1
+  type        = string
+  default     = "ami-0c02fb55956c7d316"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
+  type        = string
   default     = "t2.micro"
 }
